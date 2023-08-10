59d6492b69abe8598cfa107b6bb984fde1c91fd9
LaurentiuTusa <lautusa@yahoo.com>
2023-08-10 13:05:56 +0300

    improved dropdown, finish logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   get 'users/new'
   root "home#index"
   resources :about, only: :index
-  get 'contact', to: 'contact#index'
-  get 'service', to: 'service#index'
-  get 'blog', to: 'blog#index'
+  resources :contact, only: :index
+  resources :service, only: :index
+  resources :blog, only: :index
 
   get 'apply_filters', to: 'home#apply_filters', as: :apply_filters
 
   get '/signup', to: 'users#new'
-  get 'show', to: 'users#show'
+  get '/show', to: 'users#show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   resources :users
 end
